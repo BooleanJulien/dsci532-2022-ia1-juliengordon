869899f0a6d29dd1d87116650ef70733efcb7463
Julien Gordon <junigord@gmail.com>
2022-02-19 13:20:59 -0800

    saving before creating heroku

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ gap_data = pd.read_csv('world-data-gapminder_raw.csv')
 
 gap_data = gap_data.groupby(['sub_region', 'year']).mean()
 
+app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='ycol-widget', value='life_expectancy',
+        options=[{'label': i, 'value': i} for i in gap_data.columns]),
+        html.Iframe(
+            id='scatter',
+            style={'border-width': '0', 'width': '100%', 'height': '400px'})])
+
+@app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('ycol-widget', 'value'))
 def plot_altair(ycol):
     chart = alt.Chart(gap_data.reset_index()).mark_line().encode(
     x='year',
@@ -15,26 +27,10 @@ def plot_altair(ycol):
     tooltip='sub_region').interactive()
     return chart.to_html()
 
-app = Dash(__name__)
-
-app.layout = html.Div([
-    dcc.Dropdown(
-        id='ycol', value='life_expectancy',
-        options=[{'label': i, 'value': i} for i in gap_data.columns]),
-        html.Iframe(
-            id='scatter',
-            style={'border-width': '0', 'width': '100%', 'height': '400px'},
-            srcDoc=plot_altair(ycol='life_expectancy'))])
-
-@app.callback(
-    Output('scatter', 'srcDoc'),
-    Input('ycol', 'value'))
-
 def update_output(ycol):
     return plot_altair(ycol)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
 
 
